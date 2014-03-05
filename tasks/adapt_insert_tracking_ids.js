1d5fb7378a11039c46f3f0598365be3b82fca15f
Kev Adsett <kev.adsett@gmail.com>
2014-03-05 11:04:04 +0000

    Fix logic in tracking ID replacement

--- tasks/adapt_insert_tracking_ids.js
+++ tasks/adapt_insert_tracking_ids.js
@@ -1,9 +1,6 @@
 /*
  * grunt-adapt-tracking-ids
- * https://github.com/kevadsett/trackingIds-plugin
- *
- * Copyright (c) 2013 Kev Adsett
- * Licensed under the MIT license.
+ * https://github.com/kevadsett/adapt-grunt-trackingIds
  */
 
 'use strict';
@@ -22,13 +19,16 @@ module.exports = function(grunt) {
             
             for(var i = 0; i < blocks.length; i++) {
                 var block = blocks[i];
+                grunt.log.writeln("block: " + block._id + ": " + (block._trackingId !== undefined ? block._trackingId : "not set"));
                 if(block._trackingId === undefined) {
                     block._trackingId = ++options._latestTrackingId;
                     grunt.log.writeln("Adding tracking ID: " + block._trackingId + " to block " + block._id);
                 } else {
                     if(options._trackingIdsSeen.indexOf(block._trackingId) > -1) {
-                        grunt.log.writeln("Warning: " + block._id + " has the tracking ID " + block._id + ", but this is already in use. Changing to " + (options._latestTrackingId + 1) + ".");
+                        grunt.log.writeln("Warning: " + block._id + " has the tracking ID " + block._trackingId + ", but this is already in use. Changing to " + (options._latestTrackingId + 1) + ".");
                         block._trackingId = ++options._latestTrackingId;
+                    } else {
+                        options._trackingIdsSeen.push(block._trackingId);
                     }
                 }
                 if(options._latestTrackingId < block._trackingId) {
@@ -37,7 +37,7 @@ module.exports = function(grunt) {
                     
             }
             course._latestTrackingId = options._latestTrackingId;
-            grunt.log.writeln("The latest tracking ID is " + course._latestTrackingId);
+            grunt.log.writeln("Task complete. The latest tracking ID is " + course._latestTrackingId);
             grunt.file.write(options.courseFile, JSON.stringify(course, null, "    "));
             grunt.file.write(options.blocksFile, JSON.stringify(blocks, null, "    "));
         }
